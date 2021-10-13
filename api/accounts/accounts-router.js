fd4acb759805d3aca941a977448c2a9a315c89a1
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-10-13 12:09:23 -0700

    completed both get req

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,46 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const Accounts = require("./accounts-model");
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
+  Accounts.getAll()
+    .then((acc) => {
+      res.status(200).json(acc);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
-router.get('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+  const { id } = req.params;
+  Accounts.getById(id).then((acc) => {
+    if (acc) {
+      res.status(200).json(acc);
+    } else {
+      res.status(404).json({
+        message: "account not found",
+      });
+    }
+  });
+});
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
+router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
 });
 
-router.delete('/:id', (req, res, next) => {
+router.put("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.delete("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  // DO YOUR MAGIC
+});
 
 module.exports = router;
